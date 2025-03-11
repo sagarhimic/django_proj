b6015b34874667ecba2367818b44eeb95c1795fe
sagarhimic <121603724+sagarhimic@users.noreply.github.com>
2025-03-11 15:13:51 +0530

    Update settings.py

--- fbvproject/fbvproject/settings.py
+++ fbvproject/fbvproject/settings.py
@@ -78,11 +78,19 @@ WSGI_APPLICATION = 'fbvproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'  : 'django.db.backends.mysql',
+        'NAME'    : 'python_db',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # Supports special characters
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
