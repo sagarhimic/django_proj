1b8586e72f2771bae199bb9f69b7e1b3aff0e2e0
sagarhimic <121603724+sagarhimic@users.noreply.github.com>
2025-03-11 16:03:12 +0530

    Update wsgi.py

--- fbvproject/fbvproject/wsgi.py
+++ fbvproject/fbvproject/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+# Add your project directory to the sys.path
+path = "/home/sagarc/django_proj/fbvproject"
+if path not in sys.path:
+    sys.path.append(path)
+
+# Set the settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fbvproject.settings")
+
+# Import and set the application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fbvproject.settings')
-
 application = get_wsgi_application()
