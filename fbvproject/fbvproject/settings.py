1b1023bb1952e2b26fbe16ebed504b558ec659a5
sagarhimic <121603724+sagarhimic@users.noreply.github.com>
2025-03-11 15:10:48 +0530

    Update settings.py

--- fbvproject/fbvproject/settings.py
+++ fbvproject/fbvproject/settings.py
@@ -80,12 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        #'ENGINE'  : 'django.db.backends.mysql',
-        #'NAME'    : 'python_db',
-        #'USER':'root',
-        #'PASSWORD':'',
-        #'HOST' : 'localhost',
-        #'PORT' : '3306'
     }
 }
 
